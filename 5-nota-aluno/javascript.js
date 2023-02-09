5ab42e46d4674b7d6bae159c8e39d5f8c59b5851
rickshawwalis <rickshawwallis@gmail.com>
2023-02-09 18:34:30 -0300

    a

--- 5-nota-aluno/javascript.js
+++ 5-nota-aluno/javascript.js
@@ -7,12 +7,15 @@ let notaAluno = () => {
   let res = document.querySelector("#res");
   let media = (Number(nota1) + Number(nota2)) / 2
 
-
-  res.innerHTML = " ";
-  res.innerHTML += `<p>Nota 1 = ${nota1}</p>`
-  res.innerHTML += `<p>Nota 2 = ${nota2}</p>`
-  res.innerHTML += `<p>MÉDIA = (${nota1} + ${nota2}) / 2 =  ${media}</p>`
-  res.innerHTML += `<p>O aluno ${nomeAluno}, ficou com média ${media}</p>`
-  res.style.textAlign = 'justify'
-  
+  if (nomeAluno.lenght==0 || nota1==0 || nota2==0) {
+alert("Preencha os campos")
+  }
+  else {
+    res.innerHTML = " ";
+    res.innerHTML += `<p>Nota 1 = ${nota1}</p>`
+    res.innerHTML += `<p>Nota 2 = ${nota2}</p>`
+    res.innerHTML += `<p>MÉDIA = (${nota1} + ${nota2}) / 2 =  ${media}</p>`
+    res.innerHTML += `<p>O aluno ${nomeAluno}, ficou com média ${media}</p>`
+    res.style.textAlign = 'justify'
+  }
 }; button.addEventListener("click", notaAluno)
