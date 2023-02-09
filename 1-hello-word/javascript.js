0cb0fe3b1a810478237a745c0d4e5ffacbb8ee3a
rickshawwalis <rickshawwallis@gmail.com>
2023-02-09 15:44:16 -0300

    a

--- 1-hello-word/javascript.js
+++ 1-hello-word/javascript.js
@@ -6,7 +6,7 @@ function teste(){
   let res = document.querySelector("#res");
 
 if(ex1.length===0){
-  alert("Preencha o Campo NOME!")
+  alert("Preencha o Campo Abaixo!")
 } else {
   res.innerHTML = "";
   res.innerHTML += `<p>HELLO WORD!!! <br> Seja Bem-Vindo ${ex1.toUpperCase()}!</p>`;
--- 2-boasvindas/javascript.js
+++ 2-boasvindas/javascript.js
@@ -1,10 +1,14 @@
 function teste() {
   let button = document.querySelector("#button");
-    let ex1 = document.querySelector("#ex1").value;
-    let res = document.querySelector("#res");
-  
-    res.innerHTML = `Olá! ${ex1.toUpperCase()}, é um prazer te conhecer`;
-    
-  } button.addEventListener("click",teste)
+  let ex1 = document.querySelector("#ex1").value;
+  let res = document.querySelector("#res");
 
-  
+  if (ex1.length===0) {
+    alert("Preencha o campo Abaixo!")
+  } else {
+    res.innerHTML = " "
+    res.innerHTML = `Olá! ${ex1.toUpperCase()}, é um prazer te conhecer`;
+  }
+
+} button.addEventListener("click", teste)
+
