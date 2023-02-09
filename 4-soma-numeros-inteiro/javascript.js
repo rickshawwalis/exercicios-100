c63569101460dd84d5cd2699f5f6a88e8f94cd52
rickshawwalis <rickshawwallis@gmail.com>
2023-02-09 18:27:56 -0300

    s

--- 4-soma-numeros-inteiro/javascript.js
+++ 4-soma-numeros-inteiro/javascript.js
@@ -5,7 +5,7 @@ function teste() {
   const numeroRes = Number(numero1) + Number(numero2)
   const res = document.querySelector("#res")
 
-  if(numero1.lenght!==0 || numero2.lenght!==0){
+  if(numero1.lenght==0 || numero2.lenght==0){
 alert("Preencha os campos abaixo!")
   } else{
     res.innerHTML = " "
