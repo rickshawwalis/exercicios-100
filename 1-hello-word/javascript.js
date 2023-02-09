cbc8c353b9e5f973e0fab78fb75a7d0a786b1ac8
rickshawwalis <rickshawwallis@gmail.com>
2023-02-09 09:31:06 -0300

    Update javascript.js

--- 1-hello-word/javascript.js
+++ 1-hello-word/javascript.js
@@ -1,7 +1,12 @@
 function teste() {
-    let ex1 = document.querySelector("#ex1").value;
-    let res = document.querySelector("#res");
-  
-    res.innerHTML = `HELLO WORD!!! <br> SEJA BEM VINDO ${ex1.toUpperCase()}`;
+  let ex1 = document.querySelector("#ex1").value;
+  let res = document.querySelector("#res");
+
+  if(ex1.length === 0){
+    alert("ESCREVA UMA INFORMAÇÃO VÁLIDA");
+  } else {
+    res.innerHTML = `HELLO WORD!!! <br> SEJA BEM-VINDO ${ex1.toUpperCase()}`;
   }
+}
+
   
