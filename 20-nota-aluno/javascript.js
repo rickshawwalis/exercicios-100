fc709cfd724a981275cef766fb16266e93673872
rickshawwalis <rickshawwallis@gmail.com>
2023-02-12 01:16:19 -0300

    a

--- 20-nota-aluno/javascript.js
+++ 20-nota-aluno/javascript.js
@@ -1,21 +1,18 @@
-let imparPar=()=>{
+let imparPar = () => {
+  let numero = Number(document.querySelector("#numero").value);
+  let res = document.querySelector("#res");
 
-let numero = Number(document.querySelector("#numero").value);
-let res = Number(document.querySelector("#res").value);
+  if (numero == "") {
+    alert("Preencha o campo!");
+    return;
+  }
 
-if(numero == ""){
-  alert("Preencha o campo!");
-  return;
-}
-
-if(numero%2==0){
-  return par;
-} else {
-return imparPar;  
-}
-
-
+  if (numero % 2 === 0) {
+    res.innerHTML = "PAR";
+  } else {
+    res.innerHTML = "IMPAR";
+  }
 }
 
 let botao = document.querySelector("#button");
-botao.addEventListener("click",imparPar);
+botao.addEventListener("click", imparPar);
