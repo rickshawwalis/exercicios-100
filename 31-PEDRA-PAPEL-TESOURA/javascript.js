d15fad4083c68e2310b532d3e81068563828ca83
rickshawwalis <rickshawwallis@gmail.com>
2023-02-14 16:34:45 -0300

    dd

--- 31-PEDRA-PAPEL-TESOURA/javascript.js
+++ 31-PEDRA-PAPEL-TESOURA/javascript.js
@@ -9,34 +9,47 @@ let desconto = () => {
 
   if (fsex[0].checked) {
     tipo = "PEDRA";
+
     res.innerHTML += `VOCÊ ESCOLHEU ${tipo}`;
-    img.setAttribute("src", "./pedra.jpg")
+    img.setAttribute("src", "./jokenpo/pedra.jpg")
+
     res.innerHTML += "Eu escolho papel"
-    img.setAttribute("src", "./papel.jpg")
+    img.setAttribute("src", "./jokenpo/papel.jpg")
+
     res.innerHTML += "Papel envolve pedra. Ganhei!"
-    img.setAttribute("src", "./papel-enrola-pedra.png")
+    img.setAttribute("src", "./jokenpo/papel-enrola-pedra.png")
+
     res.style.textAlign = 'justify';
+    document.body.style.background = "#902D01";
 
 
   } else if (fsex[1].checked) {
     tipo = "PAPEL";
     res.innerHTML += `VOCÊ ESCOLHEU ${tipo}`;
-    img.setAttribute("src", "./papel.jpg")
+    img.setAttribute("src", "./jokenpo/papel.jpg")
+
     res.innerHTML += "Eu escolho tesoura"
-    img.setAttribute("src", "./tesoura.jpg")
+    img.setAttribute("src", "./jokenpo/tesoura.jpg")
+
     res.innerHTML += "Tesoura corta papel, ganhei!"
-    img.setAttribute("src", "./tesoura-cortapapel.jpg")
+    img.setAttribute("src", "./jokenpo/tesoura-cortapapel.jpg")
+
     res.style.textAlign = 'justify';
+    document.body.style.background = "#172D18";
 
   } else if (fsex[2].checked) {
     tipo = "TESOURA";
     res.innerHTML += `VOCÊ ESCOLHEU ${tipo}`;
-    img.setAttribute("src", "./tesoura.jpg")
+    img.setAttribute("src", "./jokenpo/tesoura.jpg")
+
     res.innerHTML += "Eu escolho pedra"
-    img.setAttribute("src", "./pedra.jpg")
+    img.setAttribute("src", "./jokenpo/pedra.jpg")
+
     res.innerHTML += "Pedra quebra tesoura, ganhei!"
-    img.setAttribute("src", "./pedra-quebra-tesoura.jpg")
+    img.setAttribute("src", "./jokenpo/pedra-quebra-tesoura.jpg")
+
     res.style.textAlign = 'justify';
+    document.body.style.background = "#992D77";
   }
   res.innerHTML = " "
   res.appendChild(img);
