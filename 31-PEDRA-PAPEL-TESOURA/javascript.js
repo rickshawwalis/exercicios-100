c0c583cabefc37c68590ae12db799620764467a8
rickshawwalis <rickshawwallis@gmail.com>
2023-02-14 20:38:28 -0300

    aa

--- 31-PEDRA-PAPEL-TESOURA/javascript.js
+++ 31-PEDRA-PAPEL-TESOURA/javascript.js
@@ -7,63 +7,81 @@ let desconto = () => {
 
   var img1 = document.createElement('img');
   img1.setAttribute("id", "fotos");
-  var img2 = document.createElement('img');
-  img2.setAttribute("id", "fotos");
-  var img3 = document.createElement('img');
-  img3.setAttribute("id", "fotos");
 
-  let opcoes = ["PEDRA", "PAPEL", "TESOURA", "MATEMÁTICA"];
+  let opcoes = ["PEDRA", "PAPEL", "TESOURA"];
   let escolha = opcoes[Math.floor(Math.random() * opcoes.length)];
 
   if (fsex[0].checked) {
     tipo = "PEDRA";
     res.innerHTML = ""
-    res.innerHTML += `<p>VOCÊ ESCOLHEU ${tipo}</p>`;
-    img1.setAttribute("src", "./jokenpo/pedra.jpg")
-
-    res.innerHTML += "<p>Eu escolho papel</p>"
-    img2.setAttribute("src", "./jokenpo/papel.jpg")
-
-    res.innerHTML += "<p>Papel envolve pedra. Ganhei!</p>"
-    img3.setAttribute("src", "./jokenpo/papel-enrola-pedra.png")
-
+    res.innerHTML =
+      `<p>->VOCÊ ESCOLHEU ${tipo}</p>
+    <p>->EU ESCOLHO ${escolha}</p>`;
+   
+    if (tipo == "PEDRA" && escolha == "PEDRA") {
+      res.innerHTML += "EMPATE !!!"
+      img1.setAttribute("src", "./jokenpo/pedra.pedra.jpg")
+   
+    } else if (tipo == "PEDRA" && escolha == "PAPEL") {
+      res.innerHTML += "GANHEI !!!"
+      img1.setAttribute("src", "./jokenpo/papevspedra.jpg")
+   
+    } else if (tipo == "PEDRA" && escolha == "TESOURA") {
+      res.innerHTML += "PERDI !!!"
+      img1.setAttribute("src", "./jokenpo/pedravstesoura.jpg")
+    }
     res.style.textAlign = 'justify';
     document.body.style.background = "#902D01";
-
 
   } else if (fsex[1].checked) {
     tipo = "PAPEL";
     res.innerHTML = ""
-    res.innerHTML += `<p>VOCÊ ESCOLHEU ${tipo}</p>`;
-    img1.setAttribute("src", "./jokenpo/papel.jpg")
+    res.innerHTML =
+      `<p>->VOCÊ ESCOLHEU ${tipo}</p>
+    <p>->EU ESCOLHO ${escolha}</p>`;
+   
+    if (tipo == "PAPEL" && escolha == "PEDRA") {
+      res.innerHTML += "PERDI !!!"
+      img1.setAttribute("src", "./jokenpo/papevspedra.jpg")
+  
+    } else if (tipo == "PAPEL" && escolha == "PAPEL") {
+      res.innerHTML += "EMPATE !!!"
+      img1.setAttribute("src", "./jokenpo/papelVSpapel.jpg")
 
-    res.innerHTML += "<p>Eu escolho tesoura</p>"
-    img2.setAttribute("src", "./jokenpo/tesoura.jpg")
 
-    res.innerHTML += "<p>Tesoura corta papel. Ganhei!</p>"
-    img3.setAttribute("src", "./jokenpo/tesoura-cortapapel.jpg")
-
-    res.style.textAlign = 'justify';
+    } else if (tipo == "PAPEL" && escolha == "TESOURA") {
+      img1.setAttribute("src", "./jokenpo/tesouravspapel.jpg")
+      res.innerHTML += "GANHEI !!!"
+    }
+     res.style.textAlign = 'justify';
     document.body.style.background = "#172D18";
 
   } else if (fsex[2].checked) {
     tipo = "TESOURA";
     res.innerHTML = ""
-    res.innerHTML += `<p>VOCÊ ESCOLHEU ${tipo}</p>`;
-    img1.setAttribute("src", "./jokenpo/tesoura.jpg")
+    res.innerHTML =
+      `<p>->VOCÊ ESCOLHEU ${tipo}</p>
+    <p>->EU ESCOLHO ${escolha}</p>`;
+    
+    if (tipo == "TESOURA" && escolha == "PEDRA") {
+      img1.setAttribute("src", "./jokenpo/pedravstesoura.jpg")
+      res.innerHTML += "GANHEI !!!"
+    
+    } else if (tipo == "TESOURA" && escolha == "PAPEL") {
+      img1.setAttribute("src", "./jokenpo/tesouravspapel.jpg")
+      res.innerHTML += "PERDI !!!"
+   
+    } else if (tipo == "TESOURA" && escolha == "TESOURA") {
+      img1.setAttribute("src", "./jokenpo/tesouraVStesoura.jpg")
 
-    res.innerHTML += "<p>Eu escolho pedra</p>"
-    img2.setAttribute("src", "./jokenpo/pedra.jpg")
-
-    res.innerHTML += "<p>Pedra quebra tesoura. Ganhei!</p>"
-    img3.setAttribute("src", "./jokenpo/pedra-quebra-tesoura.jpg")
-    res.style.textAlign = 'justify';
+      res.innerHTML += "EMPATE !!!"
+    }
+       res.style.textAlign = 'justify';
     document.body.style.background = "#992D77";
 
   }
   res.appendChild(img1);
-  res.appendChild(img2);
-  res.appendChild(img3);
+
 };
 
 let botao = document.querySelector("#button");
