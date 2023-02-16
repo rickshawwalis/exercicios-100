05b4e5b459a393485ce6f21d360aed9cc5394715
rickshawwalis <rickshawwallis@gmail.com>
2023-02-16 19:03:33 -0300

    Create javascript.js

---
+++ 38-REPETICOES/javascript.js
@@ -0,0 +1,16 @@
+console.log("teste 1")
+for(let a = 6;a<=11;a++){
+  console.log(a)
+}
+
+console.log("teste 2")
+let a = 11;
+for(let i = 6;i<=a;i++){
+  console.log(i)
+}
+
+console.log("teste 3")
+let num = [6,7,8,9,10,11];
+for(n of num){
+  console.log(n)
+}
