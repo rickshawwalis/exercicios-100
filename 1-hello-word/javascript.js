dec1c0c55b1e37c4dfe1b793ba45569f572688b6
rickshawwalis <rickshawwallis@gmail.com>
2023-02-15 23:50:52 -0300

    Update javascript.js

--- 1-hello-word/javascript.js
+++ 1-hello-word/javascript.js
@@ -1,17 +1,17 @@
 
 
-function teste(){
+function teste() {
 
   let ex1 = document.querySelector("#ex1").value;
   let res = document.querySelector("#res");
 
-if(ex1.length==""){
-  alert("Preencha o Campo abaixo!")
-} else {
-  res.innerHTML = "";
-  res.innerHTML = `<p>HELLO WORD!!! <br> Seja Bem-Vindo ${ex1.toUpperCase()}!</p>`;
+  if (ex1.length == "") {
+    alert("Preencha o Campo abaixo!")
+  } else {
+    res.innerHTML = "";
+    res.innerHTML = `<p>HELLO WORD!!! <br> Seja Bem-Vindo ${ex1.toUpperCase()}!</p>`;
 
 
-}
-  
+  }
+
 }
