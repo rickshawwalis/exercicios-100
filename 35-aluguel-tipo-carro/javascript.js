d2821795b3f923f6dd32c8889160cc08d710694a
rickshawwalis <rickshawwallis@gmail.com>
2023-02-16 16:57:32 -0300

    3

--- 35-aluguel-tipo-carro/javascript.js
+++ 35-aluguel-tipo-carro/javascript.js
@@ -1,88 +1,54 @@
-let desconto = () => {
-
+let aluguel = () => {
+  let diasAlugado = Number(document.querySelector("#diasalugado").value);
+  let kmRodado = Number(document.querySelector("#kmpercorrido").value);
   let res = document.querySelector("#res");
 
-  let fsex = document.getElementsByName("jokenpo");
+  let valorDiasPopular = diasAlugado * 90
+  let valorDiasLuxo = diasAlugado * 150
+
+
+  let tipoCarro = document.getElementsByName("carroaluguel");
   let tipo = "";
 
-  var img1 = document.createElement('img');
-  img1.setAttribute("id", "fotos");
-
-  let opcoes = ["PEDRA", "PAPEL", "TESOURA"];
-  let escolha = opcoes[Math.floor(Math.random() * opcoes.length)];
-//INPUT 1
-  if (fsex[0].checked) {
-    tipo = "PEDRA";
-    res.innerHTML = ""
-    res.innerHTML =
-      `<p>->VOCÊ ESCOLHEU ${tipo}</p>
-    <p>->EU ESCOLHO ${escolha}</p>`;
-   
-    if (tipo == "PEDRA" && escolha == "PEDRA") {
-      res.innerHTML += "EMPATE !!!"
-      img1.setAttribute("src", "./jokenpo/pedra.pedra.jpg")
-   
-    } else if (tipo == "PEDRA" && escolha == "PAPEL") {
-      res.innerHTML += "GANHEI !!!"
-      img1.setAttribute("src", "./jokenpo/papevspedra.jpg")
-   
-    } else if (tipo == "PEDRA" && escolha == "TESOURA") {
-      res.innerHTML += "PERDI !!!"
-      img1.setAttribute("src", "./jokenpo/pedravstesoura.jpg")
-    }
-    res.style.textAlign = 'justify';
-    document.body.style.background = "#902D01";
-//INPUT 2
-  } else if (fsex[1].checked) {
-    tipo = "PAPEL";
-    res.innerHTML = ""
-    res.innerHTML =
-    `<p>->VOCÊ ESCOLHEU ${tipo}</p>
-    <p>->EU ESCOLHO ${escolha}</p>`;
-   
-    if (tipo == "PAPEL" && escolha == "PEDRA") {
-      res.innerHTML += "PERDI !!!"
-      img1.setAttribute("src", "./jokenpo/papevspedra.jpg")
-  
-    } else if (tipo == "PAPEL" && escolha == "PAPEL") {
-      res.innerHTML += "EMPATE !!!"
-      img1.setAttribute("src", "./jokenpo/papelVSpapel.jpg")
-
-
-    } else if (tipo == "PAPEL" && escolha == "TESOURA") {
-      img1.setAttribute("src", "./jokenpo/tesouravspapel.jpg")
-      res.innerHTML += "GANHEI !!!"
-    }
-     res.style.textAlign = 'justify';
-    document.body.style.background = "#172D18";
-
-//INPUT 3
-  } else if (fsex[2].checked) {
-    tipo = "TESOURA";
-    res.innerHTML = ""
-    res.innerHTML =
-      `<p>->VOCÊ ESCOLHEU ${tipo}</p>
-    <p>->EU ESCOLHO ${escolha}</p>`;
-    
-    if (tipo == "TESOURA" && escolha == "PEDRA") {
-      img1.setAttribute("src", "./jokenpo/pedravstesoura.jpg")
-      res.innerHTML += "GANHEI !!!"
-    
-    } else if (tipo == "TESOURA" && escolha == "PAPEL") {
-      img1.setAttribute("src", "./jokenpo/tesouravspapel.jpg")
-      res.innerHTML += "PERDI !!!"
-   
-    } else if (tipo == "TESOURA" && escolha == "TESOURA") {
-      img1.setAttribute("src", "./jokenpo/tesouraVStesoura.jpg")
-      res.innerHTML += "EMPATE !!!"
-    }
-       res.style.textAlign = 'justify';
-    document.body.style.background = "#992D77";
-
+  if (diasAlugado == "" || kmRodado == "") {
+    alert("Preencha os campos!");
+    return;
   }
-  res.appendChild(img1);
 
+  if (tipoCarro[0].checked) {
+    tipo = "CARRO POPULAR";
+    if (kmRodado <= 100) {
+      res.innerHTML =
+        ` <p>Foi escolhido: ${tipo} </p>
+        <p>O carro ficou: ${diasAlugado} dia(s) alugado, ao valor de R$ 90,00, totalizando ${valorDiasPopular}</p>
+           <p> Foram rodados ${kmRodado} km, ao valor de R$ 0.20 por km, totalizando R$ ${(kmRodado * 0.20).toFixed(2)}</p>
+           <p>Valor Total (dias e km): ${(valorDiasPopular + (kmRodado * 0.20)).toFixed(2)}</p>`
+    } else if (kmRodado > 100) {
+      res.innerHTML =
+        ` <p>Foi escolhido: ${tipo} </p>
+        <p>O carro ficou: ${diasAlugado} dia(s) alugado, ao valor de R$ 90,00, totalizando ${valorDiasPopular}</p>
+           <p> Foram rodados ${kmRodado} km, ao valor de R$ 0.10 por km, totalizando R$ ${(kmRodado * 0.10).toFixed(2)}</p>
+           <p>Valor Total (dias e km): R$  ${(valorDiasPopular + (kmRodado * 0.10)).toFixed(2)}</p>
+           `
+    }
+  } else if (tipoCarro[1].checked) {
+    tipo = "CARRO DE LUXO";
+
+   if (kmRodado <= 200) {
+    res.innerHTML =
+      ` <p>Foi escolhido: ${tipo} </p>
+    <p>O carro ficou: ${diasAlugado} dia(s) alugado, ao valor de R$ 150,00, totalizando ${valorDiasLuxo}</p>
+       <p> Foram rodados ${kmRodado} km, ao valor de R$ 0.30 por km, totalizando R$ ${(kmRodado * 0.30).toFixed(2)}</p>
+       <p>Valor Total (dias e km): ${(valorDiasLuxo + (kmRodado * 0.30)).toFixed(2)}</p>`
+  } else if (kmRodado > 200) {
+    res.innerHTML =
+      ` <p>Foi escolhido: ${tipo} </p>
+    <p>O carro ficou: ${diasAlugado} dia(s) alugado, ao valor de R$ 150,00, totalizando ${valorDiasLuxo}</p>
+       <p> Foram rodados ${kmRodado} km, ao valor de R$ 0.25 por km, totalizando R$ ${(kmRodado * 0.25).toFixed(2)}</p>
+       <p>Valor Total (dias e km): R$ ${(valorDiasLuxo + (kmRodado * 0.25)).toFixed(2)}</p>
+       `
+  }
+}
 };
-
 let botao = document.querySelector("#button");
-botao.addEventListener("click", desconto);
+botao.addEventListener("click", aluguel);
