06672605ef6b8c4909b9933f2702614c01dc19e5
rickshawwalis <rickshawwallis@gmail.com>
2023-02-17 10:07:57 -0300

    k

---
+++ 39-SEM_HTML-REPETICOES/javascript.js
@@ -0,0 +1,18 @@
+
+console.log("teste 1")
+for(let a = 10;a>=3;a--){
+  console.log(a)
+}
+
+
+console.log("teste 2")
+let a = 3;
+for(let i = 10;i>=a;i--){
+  console.log(i)
+}
+
+console.log("teste 3")
+let num = [10,9,8,7,6,5,4,3];
+for(n of num){
+  console.log(n)
+}
---
+++ 40-SEM_HTML-REPETICOES/javascript.js
@@ -0,0 +1,19 @@
+
+/*console.log("teste 1")
+for(let a = 10;a>=3;a--){
+  console.log(a)
+}
+*/
+
+console.log("teste 2")
+let a = 18;
+for(let i = 0;i<a;i++){
+  i += 3;
+  console.log(i)
+}
+/* 
+console.log("teste 3")
+let num = [0, 3, 6, 9, 12, 15, 18];
+for(n of num){
+  console.log(n)
+}*/
