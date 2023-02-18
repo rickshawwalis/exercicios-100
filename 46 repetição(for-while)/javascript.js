d29511db3230fde5498560d080859e23349f95fd
rickshawwalis <rickshawwallis@gmail.com>
2023-02-18 00:48:41 -0300

    a

--- 46 repetição(for-while)/javascript.js
+++ 46 repetição(for-while)/javascript.js
@@ -1,5 +1,18 @@
 let soma = 0;
 for (let i = 6; i <= 100; i += 2) {
+ document.write(`${i} `)
   soma += i;
 }
-console.log(soma);
+document.write(`<br><br>Soma: ${soma}`);
+
+/*
+let soma = 0;
+let i = 6;
+while (i <= 100) {
+  soma += i;
+  console.log(i);
+  i += 2;
+}
+console.log("A soma dos números pares de 6 a 100 é: " + soma);
+
+*/
