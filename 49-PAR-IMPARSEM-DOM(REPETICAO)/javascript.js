a4e0916096a90ef2a8846269844344b3b7a9733b
rickshawwalis <rickshawwallis@gmail.com>
2023-02-18 02:31:01 -0300

    Update javascript.js

--- 49-PAR-IMPARSEM-DOM(REPETICAO)/javascript.js
+++ 49-PAR-IMPARSEM-DOM(REPETICAO)/javascript.js
@@ -1,17 +1,42 @@
-let soma = 0;
+let somaPar = 0;
+let somaImpar = 0;
 
 for (let i = 1; i <= 6; i++) {
   let numero = parseInt(prompt(`Digite o ${i}º número:`));
   
-  if (numero % 2 == 0) {
-    document.write(`<br>Número par: ${numero}`)
-    soma += numero;
-    document.write(`<br>Soma números par: ${soma}`)
-  } else if (numero % 2 == 1) {
-    document.write(`<br>Número impar: ${numero}`)
-    soma += numero;
-    document.write(`<br>Soma números impar: ${soma}`)
+  if (numero % 2 === 0) {
+    document.write(`<br>Número par: ${numero}`);
+    somaPar += numero;
+
+  } else {
+    document.write(`<br>Número ímpar: ${numero}`);
+    somaImpar += numero;
   }
 }
 
+document.write(`<br><br>Soma números par: ${somaPar}`);
+document.write(`<br>Soma números ímpares: ${somaImpar}`);
 
+/* USANDO WHILE
+let somaPar = 0;
+let somaImpar = 0;
+let i = 1;
+
+while (i <= 6) {
+  let numero = parseInt(prompt(`Digite o ${i}º número:`));
+  
+  if (numero % 2 === 0) {
+    document.write(`<br>Número par: ${numero}`);
+    somaPar += numero;
+
+  } else {
+    document.write(`<br>Número ímpar: ${numero}`);
+    somaImpar += numero;
+  }
+  
+  i++;
+}
+
+document.write(`<br><br>Soma números par: ${somaPar}`);
+document.write(`<br>Soma números ímpares: ${somaImpar}`);
+*/
