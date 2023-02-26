70fd91a5725ec29fe6d981c3d79bb3f9b5c3c867
rickshawwalis <rickshawwallis@gmail.com>
2023-02-25 23:22:34 -0300

    Update javascript.js

--- 53-IDADE-SEXO(FOR-WHILE)/javascript.js
+++ 53-IDADE-SEXO(FOR-WHILE)/javascript.js
@@ -1,22 +1,27 @@
-let numHomens = 0;
-let numMulheres = 0;
-let somaIdades = 0;
-let somaIdadesHomens = 0;
-let numMulheresMaisDe20 = 0;
+let numHomens = 0;//Quantidade de homens cadastrados
+let numMulheres = 0;//Quantidade de mulheres cadastradas
+let somaIdades = 0;//Média de idade do grupo
+let somaIdadesHomens = 0;//Média de idade dos homens
+let numMulheresMaisDe20 = 0;//Quantidade de mulheres com mais de 20 anos
 
 for (let i = 1; i <= 5; i++) {
   let idade = Number(prompt("Informe a idade da pessoa " + i));
   let sexo = prompt("Informe o sexo da pessoa " + i + " (M ou F)").toUpperCase();
   if (idade == "" || sexo !== "M" && sexo !== "F") {
+
     alert("Preencha os campos corretamente!")
+   i--; //para que o laço continue perguntando na mesma iteração
+   continue; //para que o laço vá para a próxima iteração
   }
   somaIdades += idade;
 
   if (sexo === "M") {
     numHomens++;
     somaIdadesHomens += idade;
+ 
   } else if (sexo === "F") {
     numMulheres++;
+   
     if (idade > 20) {
       numMulheresMaisDe20++;
     }
