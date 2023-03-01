4ca91325f2e543f8f39e1a936b73771b8a7976d6
rickshawwalis <rickshawwallis@gmail.com>
2023-03-01 14:25:57 -0300

    f

---
+++ 58-999-FLAG/javascript.js
@@ -0,0 +1,25 @@
+let somaIdades = 0;
+let idades = [];
+let flag = true;
+
+while (flag) {
+    let idade = Number(prompt("Digite a idade: "));
+
+    if (isNaN(idade) || idade <= 0) {
+        alert("Digite uma idade válida!");
+    }
+
+    idades.push(idade);
+
+    if (idade < 999) {
+        somaIdades += idade;
+    }
+    else if (idade === 999) {
+        flag = false;
+    }
+   //flag = confirm("Deseja continuar (OK -> SIM ou CANCELAR -> NÃO)?");
+}
+
+let mediaIdades = somaIdades / idades.length;
+document.write("Média de idade: " + mediaIdades.toFixed(2) + "<br>");
+document.write("Número de alunos: " + idades.length);
